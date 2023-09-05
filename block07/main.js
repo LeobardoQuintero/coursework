3858b368eeaf78a85d213a2701b3829fb962b6dc
LeobardoQuintero <Leobardoquintero2002@gmail.com>
2023-09-05 16:26:38 -0700

    block07

---
+++ block07/main.js
@@ -0,0 +1,14 @@
+/*pseudocode: My first step is to create a HTML file
+And to create a equations to equal 10-40-39
+And to create a button and a pop up alert
+*/
+
+
+const add = 5+5;
+const multi = 5*8;
+const minus = 40-1;
+
+
+function myFunction() {
+    alert(add + " " + multi + " " + minus);
+}
